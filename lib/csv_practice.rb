7bbdd6111129abb4c93c25fb581a946ef859b2b3
Jasmine <jasylop@gmail.com>
2020-09-17 22:02:14 -0700

    wrote methods get_all_olympic_athletes, total_medals_per_team, get_all_gold_medalists

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -2,12 +2,29 @@ require 'csv'
 require 'awesome_print'
 
 def get_all_olympic_athletes(filename)
-
+  olympic_athlete_subset = []
+  CSV.read("#{filename}", headers: true).map { |row| row.to_h }.each do |athlete|
+    olympic_athlete_subset << athlete.select do |key, value|
+      %w[ID Name Height Team Year City Sport Event Medal].include?(key)
+    end
+  end
+  return olympic_athlete_subset
 end
 
 def total_medals_per_team(olympic_data)
+  total_medals = Hash.new(0)
+  olympic_data.each do |athlete|
+    if athlete["Medal"] != "NA"
+      total_medals[athlete["Team"]] += 1
+    end
+  end
 
+  return total_medals
 end
 
+
 def get_all_gold_medalists(olympic_data)
+  olympic_data.select do |hash|
+    hash["Medal"] == "Gold"
+  end
 end
